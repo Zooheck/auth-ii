92705cd6490de5053c604a6a9be5b85f0270c9a9
Zooheck <davidscottflack@gmail.com>
2019-02-27 18:32:53 -0600

    add restricted middleware

--- helpers/userHelpers.js
+++ helpers/userHelpers.js
@@ -2,6 +2,7 @@ const db = require('../dbconfig.js');
 
 module.exports = {
     find,
+    findBy,
     findById,
     add
 }
@@ -15,6 +16,9 @@ function findById(id) {
     return db('users')
         .where({ id })
         .first();
+}
+function findBy(filter) {
+    return db('users').where(filter).first();
 }
 async function add(user) {
     const [id] = await db('users').insert(user);
--- index.js
+++ index.js
@@ -36,6 +36,58 @@ server.post('/api/register', async (req, res) => {
     }
 });
 
+const generateToken = user => {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        department: user.department
+    };
+
+    const options = {
+        expiresIn: '1d',
+    };
+
+    return jwt.sign(payload, secret, options)
+}
+
+server.post('/api/login', async (req, res) => {
+    let { username, password } = req.body;
+    try {
+        const user = await UserFuncs.findBy( {username} )
+        
+        if(user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user);
+
+            res.status(200).json({
+                message: `Welcome, ${user.username}!`,
+                token
+            })
+        } else {
+            res.status(401).json({ message: 'Invalid credentials.'})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+    }
+});
+
+function restricted(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: "There was an error."})
+            } else {
+                req.decodedJwt = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({ message: "No token provided"})
+    }
+}
+
 server.get('/api/users', async (req, res) => {
     try {
         const users = await UserFuncs.find();
